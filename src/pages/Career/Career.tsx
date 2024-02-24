849ff9c2ab9da35a551d63cc59135a81a1b5f996
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-02-25 10:32:42 +1300

    complete careers page

--- src/pages/Career/Career.tsx
+++ src/pages/Career/Career.tsx
@@ -1,7 +1,13 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import HeroArea from "../../components/HeroArea";
+import SubTitle from "../../components/SubTitle";
+import { Container } from "../../components/Container";
+import { data } from "./data";
+import { Card, CardInverted, CardsWrapper } from "./styled";
+import { ThemeContext } from "styled-components";
 
 const Career: FC = () => {
+  const theme = useContext(ThemeContext);
   return (
     <div>
       <HeroArea
@@ -13,6 +19,48 @@ const Career: FC = () => {
         ]}
         imageUrl="./images/grad-photo-web-2.png"
       />
+      <Container>
+        <SubTitle
+          mainText="Permanent Software Engineer"
+          description="Where I continuously learn about developing scaling software"
+        />
+        <CardsWrapper>
+          {data
+            .filter((x) => x.type === "Work")
+            .map((x) => (
+              <Card>
+                <h3>{x.name}</h3>
+                <h2>{x.position}</h2>
+                <h4
+                  style={{
+                    color: x.dateString.includes("Present")
+                      ? theme?.colors.tertiary1
+                      : "",
+                  }}
+                >
+                  {x.dateString}
+                </h4>
+                <p>{x.summary}</p>
+              </Card>
+            ))}
+        </CardsWrapper>
+        <SubTitle
+          mainText="Internships"
+          description="Where I first learned about production grade software"
+        />
+        <CardsWrapper>
+          {data
+            .filter((x) => x.type === "Internship")
+            .map((x) => (
+              <CardInverted>
+                <h3>{x.name}</h3>
+                <h2>{x.position}</h2>
+                <h4>{x.dateString}</h4>
+                <p>{x.summary}</p>
+              </CardInverted>
+            ))}
+        </CardsWrapper>
+      </Container>
     </div>
   );
 };
