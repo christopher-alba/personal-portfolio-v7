7f528a357181cfffab36898a975207424d665dc9
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-02-24 22:27:29 +1300

    Update styled.ts

--- src/pages/About/SummaryCard/styled.ts
+++ src/pages/About/SummaryCard/styled.ts
@@ -14,4 +14,8 @@ export const MainDiv = styled("div")<MainDivProps>`
   padding: 20px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
     rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    @media (max-width: 600px){
+      height: 70vh;
+      overflow: auto;
+    }
 `;
